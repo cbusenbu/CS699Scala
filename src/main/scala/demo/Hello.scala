5f583bf69a706785b856c5dc2faa44623f77942b
Charles Busenburg <cbusenbu@gmail.com>
2016-05-25 14:16:22 -0400

    initial commit to CS699Scala

---
+++ src/main/scala/demo/Hello.scala
@@ -0,0 +1,9 @@
+package demo
+
+/**
+  * Created by Chas on 5/24/16.
+  */
+class Hello {
+  def sayHello(name: String) = s"Hello, $name!"
+
+}
---
+++ src/main/scala/hello.sc
@@ -0,0 +1,5 @@
+import demo.Hello
+
+val hello = new Hello
+
+println(hello.sayHello("Scala"))
